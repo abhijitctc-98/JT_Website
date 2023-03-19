7823b211a89ece9937202970b6a82fba8ebaca16
abhijitctc-98 <kabhijit59@gmail.com>
2023-03-19 14:11:21 +0530

    Added an At a Glance
    Added a new 'At a Glance' section on the Home Page and added the font-awesome icons against all the Team Members in the 'Our Team' component.

--- client/src/app/team/team.component.ts
+++ client/src/app/team/team.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { faLinkedin, faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent {
-
+  faLinkedin = faLinkedin;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faWhatsapp = faWhatsapp;
 }
